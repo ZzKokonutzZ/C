3fba6b5f20f7b34e42ce2011726294c18065f0c5
ZzKokonutzZ <159038783+ZzKokonutzZ@users.noreply.github.com>
2025-04-15 13:51:04 +0200

    shader test

--- shader.fs
+++ shader.fs
@@ -5,10 +5,14 @@ uniform float time;
 void main() {
     vec2 uv = gl_FragCoord.xy/resolution;
     uv=2*uv-1;
-    uv.x=uv.x*resolution.x/resolution.y;
-    float r=abs(sin(5*3.14*length(uv)-5*time));
-    float g=abs(sin(5*3.14*length(uv)-6*time));
-    float b=abs(sin(5*3.14*length(uv)-7*time));
-    float a=abs(sin(5*3.14*length(uv)-8*time));
-    fragColor=vec4(vec3,1.0);
+    float ratio=resolution.x/resolution.y;
+    uv.x=uv.x*ratio;
+    float r=sin(10*uv.x);
+    float g=sin(20*uv.x);
+    float b=sin(5*(uv.x-uv.y));
+    float a=1.0;
+    vec3 col=vec3(r,g,b);
+    if (length(col)<1.0) col=vec3(1.0);
+    else col=vec3(0.0);
+    fragColor=vec4(col,a);
 }
